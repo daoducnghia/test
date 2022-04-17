55c962839f7ace9f05ce1ac4413ab03433987558
nghiaxd <93426925+nghiaxd@users.noreply.github.com>
2022-04-17 20:45:01 +0700

    Add files via upload

---
+++ src/main/java/com/example/bloggle/BloggleApplication.java
@@ -0,0 +1,17 @@
+package com.example.bloggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+<<<<<<< HEAD
+
+=======
+>>>>>>> 25d2a53234d69928eac2f3ef1afb0de349a616ad
+public class BloggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggleApplication.class, args);
+	}
+
+}
